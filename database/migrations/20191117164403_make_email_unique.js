4a0b644c271c474336d15e1fc8d5c10c3701c799
Patrick Stevenson <pat.jon.stevenson@gmail.com>
2019-11-18 07:11:12 -0800

    wrote migration to make email unique

--- database/migrations/20191117164403_make_email_unique.js
+++ database/migrations/20191117164403_make_email_unique.js
@@ -1,12 +1,14 @@
 
 exports.up = function (knex) {
     return knex.schema.table('users', Users => {
-        Users.string('email').notNullable().unique().alter();
+        Users.dropColumn('email');
+        Users.string('email').notNullable().unique();
     })
 };
 
 exports.down = function (knex) {
     return knex.schema.table('users', Users => {
-        Users.string('email').notNullable().alter();
+        Users.dropColumn('email');
+        Users.string('email').notNullable();
     })
 };
